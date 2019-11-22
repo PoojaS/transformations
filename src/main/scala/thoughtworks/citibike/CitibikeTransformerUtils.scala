b79a45d3bc78b9622cff34095ad026bce208a672
Pooja Subramanian <poojas2406@gmail.com>
2019-11-22 12:26:52 +0530

    Compute distance between two locations using latitude and longitude

--- src/main/scala/thoughtworks/citibike/CitibikeTransformerUtils.scala
+++ src/main/scala/thoughtworks/citibike/CitibikeTransformerUtils.scala
@@ -11,8 +11,37 @@ object CitibikeTransformerUtils {
 
   implicit class StringDataset(val dataSet: Dataset[Row]) {
 
+
     def computeDistances(spark: SparkSession) = {
-      dataSet
+
+      import org.apache.spark.sql.functions.callUDF
+      import spark.implicits._
+      spark.udf.register("computeDistance", computeDistance)
+
+      val frame = dataSet.withColumn("distance", callUDF("computeDistance",
+        $"start_station_latitude", $"end_station_latitude"
+        , $"start_station_longitude", $"end_station_longitude"))
+      frame
     }
+
+    val computeDistance = (startStationLatitude: Double, endStationLatitude: Double,
+                           startStationLongitude: Double, endStationLongitude: Double) => {
+      import scala.math.{atan2, cos, pow, sin, sqrt, toRadians}
+
+      val latitudeDifference = toRadians(endStationLatitude - startStationLatitude)
+      val longitudeDifference = toRadians(endStationLongitude - startStationLongitude)
+
+      val a =
+        pow(sin(latitudeDifference / 2), 2) +
+          cos(toRadians(startStationLatitude)) * cos(toRadians(endStationLatitude)) *
+            pow(sin(longitudeDifference / 2), 2)
+
+      BigDecimal((2 * atan2(sqrt(a), sqrt(1 - a)) * EarthRadiusInM) / MetersPerMile)
+          .setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+
+    }
+
+
   }
+
 }
